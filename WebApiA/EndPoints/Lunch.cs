1455ad08c19f97eec665206d3a50d21eaaf87f67
vanessa.Lin9 <vanessa7591@gmail.com>
2025-01-02 17:25:33 +0800

    add postAsync method

--- WebApiA/EndPoints/Lunch.cs
+++ WebApiA/EndPoints/Lunch.cs
@@ -1,7 +1,11 @@
+using Newtonsoft.Json;
+
 namespace WebApiA.EndPoints;
 
 public static class Lunch
 {
+    private static HttpClient _httpClient;
+
     public static IEndpointRouteBuilder MapLunchApi(this IEndpointRouteBuilder app)
     {
         var lunchApiGroup = app.MapGroup("Lunch");
@@ -13,6 +17,32 @@ public static class Lunch
     
     private static IResult LoginToB()
     {
-        return Results.Ok();
+        var postAsync = PostAsync(new Request
+        {
+            Message = "GG login"
+        });
+        return Results.Ok(postAsync);
     }
+
+    private static async Task<Response?> PostAsync(Request request)
+    {
+        var requestContent = JsonConvert.SerializeObject(request);
+        var httpRequestMessage = new HttpRequestMessage();
+        httpRequestMessage.Content = new StringContent(requestContent);
+        httpRequestMessage.Method = HttpMethod.Post;
+        httpRequestMessage.RequestUri = new Uri("/localhost");
+
+        var response = await (await _httpClient.SendAsync(httpRequestMessage)).Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<Response>(response);
+    }
+}
+
+internal class Response
+{
+    public string Message { get; set; }
+}
+
+internal class Request
+{
+    public string Message { get; set; }
 }
